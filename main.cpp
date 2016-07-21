47fef09efdd7faac940c2c6cd929e1ddebd6cda0
Yaroslav Drugov <yardru@yandex.ru>
2016-07-21 17:29:25 +0300

    server path update

--- main.cpp
+++ main.cpp
@@ -3,12 +3,13 @@
 
 int main(int argc, char **argv)
 {
+    char server[] = "/home/box/server";
     if (argc < 7)
         return 1;
 
-    execlp("../server", "server", argv[1], argv[2], argv[3], argv[4], argv[5], argv[6], nullptr);
+    execlp(server, server, argv[1], argv[2], argv[3], argv[4], argv[5], argv[6], nullptr);
 
-    std::cerr << "open server error\n";
+    perror("open server error");
 
     return 0;
 }
